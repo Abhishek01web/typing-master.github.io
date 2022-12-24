e0b4ac2f7d0f4b3b7b410bc238bfdd83b3cc289b
abhishek chauhan <abhishek01web@gmail.com>
2022-12-24 23:13:30 +0530

    version 0.1

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 console.log("hello git");
+
+// echo "# typing-master.github.io" >> README.md
+// git init
+// git add README.md
+// git commit -m "first commit"
+// git branch -M main
+// git remote add origin https://github.com/Abhishek01web/typing-master.github.io.git
+// git push -u origin main
